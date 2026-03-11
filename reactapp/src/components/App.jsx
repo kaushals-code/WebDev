77058b797950317e37c615b2994e970f23d3de95
Kaushal Singh <kaushal21gs@gmail.com>
2026-03-11 10:23:07 +0530

    My first App with Vite

---
+++ reactapp/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>Welcome to your first React app.</p>
+      <Button />
+    </div>
+  );
+}
+
+export default App; 
